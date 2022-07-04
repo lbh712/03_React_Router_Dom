fe63bc283605d1ad3fcc6b76c137ed9f25d4f086
lbh712 <lbh712@naver.com>
2022-07-04 14:04:21 +0900

    ver 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React Router Dom</h1>
+      <ul>
+        <li><a href="/">HOME</a></li>
+        <li><a href="/topics">TOPICS</a></li>
+        <li><a href="/contact">CONTACT</a></li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/topics" element={<Topics />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="*" element={"not found"}>not found</Route>
+      </Routes>
+    </div>
+  );
+}
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
 reportWebVitals();
